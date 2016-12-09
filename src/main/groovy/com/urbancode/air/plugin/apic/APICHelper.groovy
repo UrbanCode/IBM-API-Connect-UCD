977e2b32290f6943bc31a28c3ad80b940bb29a72
Nicholas Mathison <nhmathis@us.ibm.com>
2016-12-09 17:17:25 -0500

    158737 Created new products:replace step
    New command constructor function
    
    Renamed 'publish.groovy' to 'productsPublish.groovy'
    
    Change-Id: Ic0b66a934122ed37480558e76c4d03b20db92d1b

--- src/main/groovy/com/urbancode/air/plugin/apic/APICHelper.groovy
+++ src/main/groovy/com/urbancode/air/plugin/apic/APICHelper.groovy
@@ -11,8 +11,13 @@ package com.urbancode.air.plugin.apic
 
 public class APICHelper {
 
-    // Closure to get the output from the dscmds
-    def systemOutput = ""
+    final String APIC = "apic"
+    final def isWindows = System.getProperty('os.name').contains("Windows")
+
+    // Closure to get the output from the apic commands
+    // Deprecated: Updates to apic now print expected output to terminal
+    // Use productsReplace.groovy for current example
+    String systemOutput = ""
     public def getSystemOutput = {Process proc ->
         def out = new PrintStream(System.out, true)
         def outputStream = new StringBuilder()
@@ -25,4 +30,41 @@ public class APICHelper {
         systemOutput = outputStream.toString()
     }
 
+    public List<String> constructCommand(String apicPath, List<String> properties) {
+        List<String> result = []
+
+        if (apicPath && apicPath != APIC) {
+            File apicCmd = new File(apicPath)
+            if (!apicCmd.isFile()) {
+                String newApicPath = apicPath
+                if (!apicPath.endsWith(File.separator)) {
+                    newApicPath += File.separator
+                }
+                if (isWindows) {
+                    newApicPath += "apic.cmd"
+                } else {
+                    newApicPath += "apic"
+                }
+                apicCmd = new File(newApicPath)
+                if (!apicCmd.isFile()) {
+                    throw new FileNotFoundException("[Error] The specified APIC Tool Path '${apicPath}' " +
+                        "does not exist at or within this location.")
+                }
+            }
+            result << apicCmd.getCanonicalPath()
+            result += properties
+        } else {
+            if (isWindows) {
+                result = ["cmd", "/C"]
+                // Add full command as single argument
+                result << APIC + " " + properties.join(" ")
+            }
+            else {
+                // Add full command as individual arguments
+                result << APIC
+                result += properties
+            }
+        }
+        return result
+    }
 }
